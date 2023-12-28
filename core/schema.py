0f5cd0c013d9c44312b1974581c493d99ab5fd2f
Joseandres Montesino <joseandresmontesino@gmail.com>
2023-12-27 21:06:42 -0500

    Get list books

--- core/schema.py
+++ core/schema.py
@@ -1,8 +1,17 @@
 import graphene 
+from graphene_django import DjangoObjectType
+from books.models import Book
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "description", "created_at", "updated_at")
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    books = graphene.List(BookType)
     
+    def resolve_books(self, info):
+        return Book.objects.all()
     
 schema = graphene.Schema(query=Query)
 
